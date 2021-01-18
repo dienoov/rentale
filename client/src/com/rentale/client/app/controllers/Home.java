97fc34a149ff9bad67f71c24946708d8020408d6
dienoov <alvinodien@yahoo.co.id>
2021-01-18 19:57:23 +0700

    (+) User home search bar

--- client/src/com/rentale/client/app/controllers/Home.java
+++ client/src/com/rentale/client/app/controllers/Home.java
@@ -1,4 +1,30 @@
 package com.rentale.client.app.controllers;
 
-public class Home {
+import com.jfoenix.controls.JFXRippler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Home implements Initializable {
+
+    @FXML
+    private AnchorPane scrollAnchor;
+
+    @FXML
+    private VBox cover;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        JFXRippler coverRippler = new JFXRippler(cover);
+        coverRippler.setRipplerFill(Paint.valueOf("#51d7a0"));
+
+        scrollAnchor.setLeftAnchor(coverRippler, 0.0);
+        scrollAnchor.setRightAnchor(coverRippler, 0.0);
+        scrollAnchor.getChildren().add(coverRippler);
+    }
 }

9053c90bc9fe2471a724e469e8ae2b1b9c852018
dienoov <alvinodien@yahoo.co.id>
2021-01-20 13:11:34 +0700

    (+) Dashboard switch content

--- src/com/rentale/client/app/controllers/Dashboard.java
+++ src/com/rentale/client/app/controllers/Dashboard.java
@@ -1,6 +1,8 @@
 package com.rentale.client.app.controllers;
 
+import com.jfoenix.controls.JFXButton;
 import com.rentale.client.app.helpers.FXMLHelper;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -13,13 +15,31 @@ public class Dashboard implements Initializable {
     @FXML
     private AnchorPane anchorContent;
 
+    @FXML
+    private JFXButton dashboardBtn;
+
+    @FXML
+    private JFXButton userBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        AnchorPane content = FXMLHelper.getView("admin/main");
+        setContent("admin/main");
+
+        dashboardBtn.setOnAction(event -> setContent("admin/main"));
+        userBtn.setOnAction(event -> setContent("admin/user"));
+    }
+
+    public void setConstraint(AnchorPane content) {
         anchorContent.setTopAnchor(content, 0.0);
         anchorContent.setLeftAnchor(content, 0.0);
         anchorContent.setRightAnchor(content, 0.0);
         anchorContent.setBottomAnchor(content, 0.0);
+    }
+
+    public void setContent(String fileName) {
+        AnchorPane content = FXMLHelper.getView(fileName);
+        setConstraint(content);
+        anchorContent.getChildren().clear();
         anchorContent.getChildren().add(content);
     }
 
---
+++ src/com/rentale/client/app/controllers/admin/User.java
@@ -0,0 +1,15 @@
+package com.rentale.client.app.controllers.admin;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class User implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}

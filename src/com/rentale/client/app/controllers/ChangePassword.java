c42b8365b956766255bb74e2129b78e2f5c9682c
dienoov <alvinodien@yahoo.co.id>
2021-01-25 14:42:40 +0700

    (+) Change password

--- src/com/rentale/client/app/controllers/ChangePassword.java
+++ src/com/rentale/client/app/controllers/ChangePassword.java
@@ -4,18 +4,15 @@ import com.jfoenix.controls.JFXDrawer;
 import com.rentale.client.app.helpers.FXMLHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class userPanel implements Initializable {
+public class ChangePassword implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -23,19 +20,12 @@ public class userPanel implements Initializable {
     }
 
     @FXML
-    public void changePassword(ActionEvent event) {
+    public void backToUserMenu(ActionEvent event) {
         Scene scene = (Scene) ((Node)event.getSource()).getScene();
         JFXDrawer drawer = (JFXDrawer) scene.lookup("#drawer");
 
-        AnchorPane changePassword = FXMLHelper.getView("change_password");
+        AnchorPane changePassword = FXMLHelper.getView("user_panel");
         drawer.setSidePane(changePassword);
-    }
-
-    @FXML
-    public void logOut(ActionEvent event) throws Exception {
-        Parent view = FXMLLoader.load(getClass().getResource("/com/rentale/client/resources/fxml/login.fxml"));
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.getScene().setRoot(view);
     }
 
 }

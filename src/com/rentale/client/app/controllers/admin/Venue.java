9b7c64bfef8d6708664e16d9357935b6c45ba3a8
dienoov <alvinodien@yahoo.co.id>
2021-02-01 05:46:17 +0700

    (+) Venues CRUD

--- src/com/rentale/client/app/controllers/admin/Venue.java
+++ src/com/rentale/client/app/controllers/admin/Venue.java
@@ -1,5 +1,9 @@
 package com.rentale.client.app.controllers.admin;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.rentale.client.app.config.DB;
 import com.rentale.client.app.dao.Venues;
 import com.rentale.client.app.helpers.AnchorPaneHelper;
 import com.rentale.client.app.helpers.FXMLHelper;
@@ -11,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -18,6 +23,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Venue implements Initializable {
@@ -39,33 +48,140 @@ public class Venue implements Initializable {
     private TableColumn<Venues, String> location;
 
     @FXML
+    private TableColumn<Venues, String> address;
+
+    @FXML
     private TableColumn<Venues, String> desc;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        name.setCellValueFactory(new PropertyValueFactory<Venues, String>("name"));
-        location.setCellValueFactory(new PropertyValueFactory<Venues, String>("location"));
-        desc.setCellValueFactory(new PropertyValueFactory<Venues, String>("desc"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        location.setCellValueFactory(new PropertyValueFactory<>("location"));
+        address.setCellValueFactory(new PropertyValueFactory<>("address"));
+        desc.setCellValueFactory(new PropertyValueFactory<>("desc"));
 
-        table.setItems(getVenue());
+        table.setItems(getVenues());
     }
 
-    public ObservableList<Venues> getVenue() {
+    public ObservableList<Venues> getVenues() {
         ObservableList<Venues> venues = FXCollections.observableArrayList();
-        venues.add(new Venues("Hotel", "Bali", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis eros eu sapien suscipit dignissim. Proin vel mauris efficitur, bibendum purus at, interdum arcu. Maecenas ac porttitor erat. Sed vitae diam iaculis."));
-        venues.add(new Venues("Apart", "Bandung", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi aliquet sagittis lectus, quis laoreet lorem gravida ut. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec sed erat orci. Donec."));
-        venues.add(new Venues("Hotel", "Malang", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vestibulum sodales urna in pharetra. Quisque lobortis faucibus turpis. In a metus vitae lorem vestibulum consequat eu ut nisi. Nulla sed lobortis augue."));
+        Connection connection = DB.getConnection();
+        try {
+            ResultSet rs = connection.createStatement().executeQuery("select * from venues");
+            while (rs.next())
+                venues.add(new Venues(rs.getInt("id"), rs.getString("name"), rs.getString("location"), rs.getString("address"), rs.getString("description")));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return venues;
     }
 
     @FXML
-    public void newAct(ActionEvent event) {
+    public void create(ActionEvent actionEvent) {
         double[] constraints = {250.0, 150.0};
-        managePane = ModalHelper.setModal(managePane, "admin/venue_modal", constraints);
+        AnchorPane modal = FXMLHelper.getView("admin/venue_modal");
+
+        AnchorPane form = (AnchorPane) ((ScrollPane) modal.getChildren().get(0)).getContent();
+
+        ((JFXButton) form.lookup("#save")).setOnAction(event -> save(
+                ((JFXTextField) form.lookup("#name")).getText(),
+                ((JFXTextField) form.lookup("#location")).getText(),
+                ((JFXTextField) form.lookup("#address")).getText(),
+                ((JFXTextArea) form.lookup("#description")).getText()
+        ));
+
+        managePane = ModalHelper.setModal(managePane, modal, constraints);
     }
 
-    public void closeModal(MouseEvent mouseEvent) {
-        Scene scene = (Scene) ((Node)mouseEvent.getSource()).getScene();
+    @FXML
+    public void edit(ActionEvent actionEvent) {
+        if (table.getSelectionModel().getSelectedItem() != null)
+            editModal(actionEvent);
+    }
+
+    @FXML
+    public void delete(ActionEvent actionEvent) {
+        if (table.getSelectionModel().getSelectedItem() != null)
+            delete();
+    }
+
+    public void editModal(ActionEvent actionEvent) {
+        double[] constraints = {250.0, 150.0};
+        AnchorPane modal = FXMLHelper.getView("admin/venue_modal");
+
+        AnchorPane form = (AnchorPane) ((ScrollPane) modal.getChildren().get(0)).getContent();
+
+        Venues venue = table.getSelectionModel().getSelectedItem();
+
+        JFXTextField name = (JFXTextField) form.lookup("#name");
+        name.setText(venue.getName());
+
+        JFXTextField location = (JFXTextField) form.lookup("#location");
+        location.setText(venue.getLocation());
+
+        JFXTextField address = (JFXTextField) form.lookup("#address");
+        address.setText(venue.getAddress());
+
+        JFXTextArea description = (JFXTextArea) form.lookup("#description");
+        description.setText(venue.getDesc());
+
+        ((JFXButton) form.lookup("#save")).setOnAction(event -> save(
+                venue.getId(),
+                name.getText(),
+                location.getText(),
+                address.getText(),
+                description.getText()
+        ));
+
+        managePane = ModalHelper.setModal(managePane, modal, constraints);
+    }
+
+    public void save(String name, String location, String address, String description) {
+        Connection connection = DB.getConnection();
+
+        try {
+            PreparedStatement save = connection.prepareStatement("insert into venues (name, location, address, description) values (?, ?, ?, ?)");
+            save.setString(1, name);
+            save.setString(2, location);
+            save.setString(3, address);
+            save.setString(4, description);
+            save.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    public void save(int id, String name, String location, String address, String description) {
+        Connection connection = DB.getConnection();
+
+        try {
+            PreparedStatement save = connection.prepareStatement("update venues set name = ?, location = ?, address = ?, description = ? where id = ?");
+            save.setString(1, name);
+            save.setString(2, location);
+            save.setString(3, address);
+            save.setString(4, description);
+            save.setInt(5, id);
+            save.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    public void delete() {
+        Connection connection = DB.getConnection();
+
+        try {
+            PreparedStatement delete = connection.prepareStatement("delete from venues where id = ?");
+            delete.setInt(1, (table.getSelectionModel().getSelectedItem()).getId());
+            delete.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void closeModal(Scene scene) {
         AnchorPane managePane = (AnchorPane) scene.lookup("#managePane");
         managePane.getChildren().remove(managePane.lookup("#backdrop"));
     }
--- src/com/rentale/client/app/dao/Venues.java
+++ src/com/rentale/client/app/dao/Venues.java
@@ -1,33 +1,81 @@
 package com.rentale.client.app.dao;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Venues {
 
-    private String name;
-    private String location;
-    private String desc;
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty name;
+    private SimpleStringProperty location;
+    private SimpleStringProperty address;
+    private SimpleStringProperty desc;
 
-    public Venues() {
-        this.name = "";
-        this.location = "";
-        this.desc = "";
+    public Venues(int id, String name, String location, String address, String desc) {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.location = new SimpleStringProperty(location);
+        this.address = new SimpleStringProperty(address);
+        this.desc = new SimpleStringProperty(desc);
     }
 
-    public Venues(String name, String location, String desc) {
-        this.name = name;
-        this.location = location;
-        this.desc = desc;
+    public int getId() {
+        return id.get();
     }
 
-    public String getName(){
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
         return name;
     }
 
-    public String getLocation(){
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getLocation() {
+        return location.get();
+    }
+
+    public SimpleStringProperty locationProperty() {
         return location;
     }
 
-    public String getDesc(){
+    public void setLocation(String location) {
+        this.location.set(location);
+    }
+
+    public String getAddress() {
+        return address.get();
+    }
+
+    public SimpleStringProperty addressProperty() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+
+    public String getDesc() {
+        return desc.get();
+    }
+
+    public SimpleStringProperty descProperty() {
         return desc;
     }
 
+    public void setDesc(String desc) {
+        this.desc.set(desc);
+    }
 }
